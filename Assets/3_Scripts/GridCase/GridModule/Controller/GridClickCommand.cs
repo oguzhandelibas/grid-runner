048043f0756f3ff64ac02875a970ed5ca583284e
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-05-10 19:44:04 +0300

    +Refactor: GridClickCommand

--- Assets/3_Scripts/GridCase/GridModule/Controller/GridClickCommand.cs
+++ Assets/3_Scripts/GridCase/GridModule/Controller/GridClickCommand.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 
 public class GridClickCommand : IGetPoolObject, IReleasePoolObject
 {
-    private readonly int _scoreRaiseAmount = 1;
+    private const int ScoreRaiseAmount = 1;
     private List<GridSquareBackground> _neighbors = new List<GridSquareBackground>();
     private ParticleSystem _matchParticle;
     private ParticleSystem _clickParticle;
@@ -21,53 +21,88 @@ public class GridClickCommand : IGetPoolObject, IReleasePoolObject
     }
     public void Click()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if (!TryGetGridSquareBackground(out var gridSquareBackground)) return;
+
+        if (gridSquareBackground.AvaibleType == AvaibleType.Lock) return;
+
+        ActivateGuruAndLock(gridSquareBackground);
+
+        _neighbors.Clear();
+        CheckNeighbors(gridSquareBackground);
+
+        if (_neighbors.Count >= 3)
         {
-            if (hit.collider.gameObject.TryGetComponent(out GridSquareBackground gridSquareManager))
+            RaiseScore();
+            ShowMatchParticles(gridSquareBackground);
+            ReleaseLockedNeighbors();
+        }
+        else
+        {
+            ShowClickParticles(gridSquareBackground);
+        }
+    }
+
+    private bool TryGetGridSquareBackground(out GridSquareBackground gridSquareBackground)
+    {
+        gridSquareBackground = null;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out var hit)) return false;
+
+        gridSquareBackground = hit.collider.gameObject.GetComponent<GridSquareBackground>();
+        return gridSquareBackground != null;
+    }
+
+    private void ActivateGuruAndLock(GridSquareBackground gridSquareBackground)
+    {
+        gridSquareBackground.guru.SetActive(true);
+        gridSquareBackground.AvaibleType = AvaibleType.Lock;
+    }
+
+    private void CheckNeighbors(GridSquareBackground gridSquareBackground)
+    {
+        if (_neighbors.Contains(gridSquareBackground)) return;
+
+        _neighbors.Add(gridSquareBackground);
+
+        foreach (var neighbor in gridSquareBackground.MyNeighbors)
+        {
+            if (neighbor.AvaibleType == AvaibleType.Lock)
             {
-                if (gridSquareManager.AvaibleType != AvaibleType.Lock)
-                {
-                    gridSquareManager.guru.SetActive(true);
-                    gridSquareManager.AvaibleType = AvaibleType.Lock;
-                    _neighbors.Clear();
-                    CheckNeighbors(gridSquareManager);
-
-                    if (_neighbors.Count >= 3)
-                    {
-                        CoreGameSignals.Instance.onUpdateGridGameScore?.Invoke(_scoreRaiseAmount);
-                        UISignals.Instance.onUpdateGridScoreText?.Invoke(1);
-                        _matchParticle.gameObject.SetActive(true);
-                        _matchParticle.transform.position = gridSquareManager.transform.position;
-                        _matchParticle.Play();
-
-                        for (int i = _neighbors.Count - 1; i >= 0; i--)
-                        {
-                            //ReleaseObject(_neighbors[i].transform.GetChild(0).gameObject, PoolType.GridCrownObject);
-                            _neighbors[i].AvaibleType = AvaibleType.Unlock;
-                            _neighbors[i].guru.SetActive(false);
-
-                        }
-                        _neighbors.Clear();
-                    }
-                    else
-                    {
-                        _clickParticle.transform.position = gridSquareManager.transform.position;
-                        _clickParticle.gameObject.SetActive(true);
-                        _clickParticle.Play();
-                    }
-                }
+                CheckNeighbors(neighbor);
             }
         }
     }
 
-    private void CheckNeighbors(GridSquareBackground gridSquareManager)
+    private void RaiseScore()
     {
-        if (_neighbors.Contains(gridSquareManager)) return;
-        _neighbors.Add(gridSquareManager);
-        for (int i = 0; i < gridSquareManager.MyNeighbors.Count; i++)
-            if (gridSquareManager.MyNeighbors[i].AvaibleType == AvaibleType.Lock)
-                CheckNeighbors(gridSquareManager.MyNeighbors[i]);
+        CoreGameSignals.Instance.onUpdateGridGameScore?.Invoke(ScoreRaiseAmount);
+        UISignals.Instance.onUpdateGridScoreText?.Invoke(ScoreRaiseAmount);
+    }
+
+    private void ShowMatchParticles(GridSquareBackground gridSquareBackground)
+    {
+        _matchParticle.gameObject.SetActive(true);
+        _matchParticle.transform.position = gridSquareBackground.transform.position;
+        _matchParticle.Play();
+    }
+
+    private void ReleaseLockedNeighbors()
+    {
+        for (var i = _neighbors.Count - 1; i >= 0; i--)
+        {
+            _neighbors[i].AvaibleType = AvaibleType.Unlock;
+            _neighbors[i].guru.SetActive(false);
+        }
+
+        _neighbors.Clear();
+    }
+
+    private void ShowClickParticles(GridSquareBackground gridSquareBackground)
+    {
+        _clickParticle.transform.position = gridSquareBackground.transform.position;
+        _clickParticle.gameObject.SetActive(true);
+        _clickParticle.Play();
     }
 
     public GameObject GetObject(PoolType poolType)
